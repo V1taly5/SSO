694e62588e524aaac4e570a1f6439fd7f8338566
V1taly5 <egorov21112061vito@gmail.com>
2023-11-19 14:32:50 +0300

    added init logger - slog

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,14 +1,57 @@
-package sso
+package main
 
-import "grpc-service/internal/config"
+import (
+	"grpc-service/internal/config"
+	"grpc-service/internal/config/lib/logger/handlers/slogpretty"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
 
 func main() {
 	// TODO: инициализировать объект конфига
 	cfg := config.MustLoad()
 
 	// TODO: инициализировать логгер
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application",
+		slog.String("env", cfg.Env),
+		slog.Int("port", cfg.GRPC.Port))
 
 	// TODO: инициализировать приложение (app)
 
 	// TODO: запустить gRPC-сервер приложения
+}
+
+func setupLogger(env string) *slog.Logger {
+
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
