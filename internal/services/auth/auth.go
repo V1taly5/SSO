7f8cb16d40726824405d231d4d547ca79268e04f
V1taly5 <egorov21112061vito@gmail.com>
2023-11-24 13:44:32 +0300

    added error handling

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -16,6 +16,8 @@ import (
 
 var (
 	ErrInvalidCredentials = errors.New("invalid credintials")
+	ErrInvalidAppID       = errors.New("invalid app id")
+	ErrUserExists         = errors.New("user already exists")
 )
 
 type Auth struct {
@@ -117,6 +119,10 @@ func (a *Auth) RegisterNewUser(ctx context.Context, email string, password strin
 
 	id, err := a.usrSaver.SaveUser(ctx, email, passHash)
 	if err != nil {
+		if errors.Is(err, storage.ErrUserExist) {
+			log.Warn("user already exists", sl.Err(err))
+			return 0, fmt.Errorf("%s: %w", op, ErrUserExists)
+		}
 		log.Error("failed to save user", sl.Err(err))
 		return 0, fmt.Errorf("%s: %w", op, err)
 	}
@@ -139,6 +145,10 @@ func (a *Auth) IsAdmin(ctx context.Context, userID int64) (bool, error) {
 
 	isAdmin, err := a.usrProvider.IsAdmin(ctx, userID)
 	if err != nil {
+		if errors.Is(err, storage.ErrAppNotFound) {
+			log.Warn("user not found", sl.Err(err))
+			return false, fmt.Errorf("%s: %w", op, ErrInvalidAppID)
+		}
 		return false, fmt.Errorf("%s: %w", op, err)
 	}
 
