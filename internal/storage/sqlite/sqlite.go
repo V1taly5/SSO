2db874e49864b240b6e819d7a5da3cbb95701ed8
V1taly5 <egorov21112061vito@gmail.com>
2023-11-24 13:43:54 +0300

    added Storage and method New

---
+++ internal/storage/sqlite/sqlite.go
@@ -0,0 +1,21 @@
+package sqlite
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+// New cereates a new instance if the SQLite storage.
+func New(storagePath string) (*Storage, error) {
+	const op = "storage.sqlite.New"
+
+	db, err := sql.Open("sqlite3", storagePath)
+	if err != nil {
+		return nil, fmt.Errorf("%s:%w", op, err)
+	}
+	return &Storage{db: db}, nil
+}
