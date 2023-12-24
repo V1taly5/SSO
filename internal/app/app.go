eb9cf30afabd0bfda43f5c7a0b6cf0cf04df5421
V1taly5 <egorov21112061vito@gmail.com>
2023-12-24 17:21:17 +0300

    added initialization storage, authService and grpcApp

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,6 +2,8 @@ package app
 
 import (
 	grpcapp "grpc-service/internal/app/grpc"
+	"grpc-service/internal/services/auth"
+	"grpc-service/internal/storage/sqlite"
 	"log/slog"
 	"time"
 )
@@ -12,10 +14,13 @@ type App struct {
 
 func New(log *slog.Logger, grpcPort int, storagePath string, tokenTTL time.Duration) *App {
 	// TODO: инициализировать хранилище (storage)
-
+	storage, err := sqlite.New(storagePath)
+	if err != nil {
+		panic(err)
+	}
 	// TODO: init auth service (auth)
-
-	grpcApp := grpcapp.New(log, grpcPort)
+	authService := auth.New(log, storage, storage, storage, tokenTTL)
+	grpcApp := grpcapp.New(log, authService, grpcPort)
 
 	return &App{
 		GRPCServer: grpcApp,
